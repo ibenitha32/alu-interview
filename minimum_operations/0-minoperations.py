45d0f9dcded3a357987b3f00afbf2ca8aea4a381
ibenitha32 <b.iradukund1@alustudent.com>
2023-09-14 17:31:48 +0000

    v

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -14,17 +14,13 @@ def min_operations(n):
     The fewest number of operations needed.
   """
 
-  if n <= 1:
-    return 0
-
-  operations = 0
-  for i in range(2, int(n ** 0.5) + 1):
-    while n % i == 0:
-      ops += i
-      n //= i
-
-  if n > 1:
-    operations += n
-
-  return operations
-
+ if n <= 1:
+        return 0
+    operations = 0
+    for i in range(2, int(math.sqrt(abs(n))) + 1):
+        while n % i == 0:
+            operations += i
+            n //= i
+    if n > 1:
+        operations += n
+    return operations
