af21171d22dd03f7fc16b30f6548326b13de97a5
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-09-16 20:54:16 +0200

    v

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
 """ Minimum operations Repo - ALU Interview Project """
 
-import math
-
-def min_operations(n):
-  """
-  Calculates the fewest number of operations needed to result in exactly n H characters in the file.
-
-  Args:
-    n: The number of H characters to be achieved.
-
-  Returns:
-    The fewest number of operations needed.
-  """
-
- if n <= 1:
+def minOperations(n):
+    """Return the fewest number of operations needed."""
+    if n == 1:
         return 0
+
     operations = 0
-    for i in range(2, int(math.sqrt(abs(n))) + 1):
-        while n % i == 0:
-            operations += i
-            n //= i
-    if n > 1:
-        operations += n
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            n //= divisor
+            operations += divisor
+        else:
+            divisor += 1
+
     return operations
