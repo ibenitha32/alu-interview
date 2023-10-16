99a7d480146fc42de773401ff0d5bb32a72f2a5f
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-10-16 21:49:36 +0200

    done

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A function that return pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+     Returns a list of lists of integers
+     representing the Pascal’s triangle of n
+     """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
